f519391485ac754e938c296694388ce25bba5819
Raita Kondo <raycondo915@gmail.com>
2023-04-03 16:09:52 +0700

    successfully added page routing feature

--- src/App.js
+++ src/App.js
@@ -6,24 +6,54 @@ import Set from "./components/Set";
 function App() {
   //---- useState for contents choice and make onClick event handler
 
-  const [activePage, setActivePage] = React.useState(true);
-  const [achievePage, setAchievePage] = React.useState(false);
-  const [setPage, setSetPage] = React.useState(false);
+  // const [activePage, setActivePage] = React.useState(true);
+  // const [achievePage, setAchievePage] = React.useState(false);
+  // const [setPage, setSetPage] = React.useState(false);
+  const [page, setPage] = React.useState({
+    activePage: true,
+    achievePage: false,
+    setPage: false,
+  });
+
+  const pageHandler = (e) => {
+    const target = e.target.value;
+
+    const newPages = {};
+
+    for (const key in page) {
+      if (key === target) {
+        newPages[key] = true;
+      } else {
+        newPages[key] = false;
+      }
+    }
+    setPage(newPages);
+  };
 
   return (
     <div className="App">
       <h1>here comes the comps</h1>
       <div className="index-nav">
-        <button className="index-card">Active</button>
-        <button className="index-card">Set</button>
-        <button className="index-card">Achieves</button>
+        <button value="activePage" onClick={pageHandler} className="index-card">
+          Active
+        </button>
+        <button value="setPage" onClick={pageHandler} className="index-card">
+          Set
+        </button>
+        <button
+          value="achievePage"
+          onClick={pageHandler}
+          className="index-card"
+        >
+          Achieves
+        </button>
       </div>
       <div className="main-container">
-        {activePage === true ? (
+        {page.activePage === true ? (
           <Active />
-        ) : achievePage === true ? (
+        ) : page.achievePage === true ? (
           <Achieves />
-        ) : setPage === true ? (
+        ) : page.setPage === true ? (
           <Set />
         ) : null}
       </div>
